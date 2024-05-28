303b1f1031b1f1487e6225b5bdac6c3716da01e7
blandskron <bastianlandskronfreelancer@gmail.com>
2024-05-28 00:44:44 -0400

    fastapi

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,9 +1,9 @@
-import jwt
+from jwt import encode, decode
 
 def create_token(data: dict) -> str:
-    token: str = jwt.encode(payload=data, key="my_secret_key", algorithm="HS256")
+    token: str = encode(payload=data, key="my_secret_key", algorithm="HS256")
     return token
 
 def validate_token(token: str) -> dict:
-    data: dict = jwt.decode(token, key="my_secret_key", algorithms=['HS256'])
+    data: dict = decode(token, key="my_secret_key", algorithms=['HS256'])
     return data
